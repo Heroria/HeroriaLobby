37fe666873782b83ebdf67d03d41e8d042f55e2e
Haiwa <clement222206@gmail.com>
2020-03-10 20:23:43 +0000

    Initialisation du projet

---
+++ [Heroria]Lobby/src/net/haiwa/lobby/Main.java
@@ -0,0 +1,16 @@
+package net.haiwa.lobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
